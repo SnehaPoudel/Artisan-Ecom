71bde7052f48c1fab9f6c0a790ef2ed5b8e2b463
SnehaPoudel <sneha.deerwalk.edu.np>
2023-09-28 11:03:28 +0545

    Initial Commit

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Dashboard = () => {
+  return <Layout title={"User Dashboard"}>Dashboard</Layout>;
+};
+
+export default Dashboard;
