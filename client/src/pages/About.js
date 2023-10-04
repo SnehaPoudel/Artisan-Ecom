43adcf1cdb240681ba368943ce6b1db338159ea6
SnehaPoudel <sneha.deerwalk.edu.np>
2023-10-04 20:34:39 +0545

    Update contact us page, categories page, and authentication page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,50 @@ import Layout from "./../components/Layout/Layout";
 const About = () => {
   return (
     <Layout title={"About us - Ecommer app"}>
-      <div className="row contactus ">
+      <div className="responsive-container-block bigContainer">
+        <div className="responsive-container-block Container">
+          <div className="responsive-container-block leftSide">
+            <p className="text-blk heading">Meet Our Creative Team</p>
+            <p className="text-blk subHeading">
+              Semaj Africa is an online education platform that delivers video
+              courses, programs and resources for Individual, Advertising &amp;
+              Media Specialist, Online Marketing Professionals, Freelancers and
+              anyone looking to pursue a career in digital marketing,
+              Accounting, Web development, Programming. Multimedia and CAD
+              design.
+            </p>
+          </div>
+          <div className="responsive-container-block rightSide">
+            <img
+              className="number1img"
+              src="/client/src/images/1.jpg"
+              alt="image1"
+            />
+            <img
+              className="number2img"
+              src="/client/src/images/2.jpg"
+              alt="image2"
+            />
+            <img
+              className="number3img"
+              src="/client/src/images/3.jpg"
+              alt="image3"
+            />
+            <img
+              className="number5img"
+              src="/client/src/images/4.jpg"
+              alt="image4"
+            />
+            <img
+              className="number7img"
+              src="/client/src/images/5.jpg"
+              alt="image5"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="row contactus ">
         <div className="col-md-6 ">
           <img
             src="/images/about.jpeg"
@@ -22,7 +65,7 @@ const About = () => {
             commodi illum quidem neque tempora nam.
           </p>
         </div>
-      </div>
+      </div> */}
     </Layout>
   );
 };
--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,20 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from "../components/Layout/Layout";
+import "../styles/allCategories.css";
+import myLocalImage1 from "../images/statue.jpg";
+import myLocalImage2 from "../images/felt.jpg";
+import myLocalImage3 from "../images/thangka.jpg";
+import myLocalImage4 from "../images/bowl.jpg";
+import myLocalImage5 from "../images/newarii.jpg";
+import myLocalImage6 from "../images/ceramics.jpg";
+
 const Categories = () => {
   const categories = useCategory();
+
+  const imageArray = [
+    myLocalImage1,
+    myLocalImage2,
+    myLocalImage3,
+    myLocalImage4,
+    myLocalImage5,
+    myLocalImage6,
+
+    // Add more image imports as needed
+  ];
+
   return (
     <Layout title={"All Categories"}>
       <div className="container" style={{ marginTop: "100px" }}>
         <div className="row container">
-          {categories.map((c) => (
+          {categories.map((c, index) => (
             <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-              <div className="card">
-                <Link to={`/category/${c.slug}`} className="btn cat-btn">
-                  {c.name}
-                </Link>
-              </div>
+              <Link to={`/category/${c.slug}`} className="category-link">
+                <div className="flip">
+                  <div
+                    className="front"
+                    style={{
+                      backgroundImage: `url(${imageArray[index]})`, // Use the imported local image
+                    }}
+                  >
+                    <h1 className="text-shadow">{c.name}</h1>
+                    {/* You can make the button invisible by setting opacity to 0 */}
+                    <p className="btn cat-btn" style={{ opacity: 0 }}>
+                      View Category
+                    </p>
+                  </div>
+                  <div className="back">
+                    <h2>{c.name}</h2>
+                    <p>
+                      Good tools make application development quicker and easier
+                      to maintain than if you did everything by hand.
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
